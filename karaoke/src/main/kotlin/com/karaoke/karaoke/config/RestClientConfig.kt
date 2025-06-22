da1c3a8297ba1c73a92dc4ab12cbd07e3bbf6d41
Heo SeokJin <pooh6195@naver.com>
2025-06-22 20:15:10 +0900

    [feat] TJ 요청을 위한 RestClient 설정 추가

---
+++ karaoke/src/main/kotlin/com/karaoke/karaoke/config/RestClientConfig.kt
@@ -0,0 +1,20 @@
+package com.karaoke.karaoke.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestClient
+
+@Configuration
+class RestClientConfig {
+
+    /**
+     * TJ미디어 검색을 위한 RestClient Bean.
+     * Bean의 이름을 "tjRestClient"로 명시하여 다른 RestClient와 구분합니다.
+     */
+    @Bean("tjRestClient")
+    fun tjRestClient(builder: RestClient.Builder): RestClient {
+        return builder
+            .baseUrl("https://www.tjmedia.com/song/accompaniment_search")
+            .build()
+    }
+}
